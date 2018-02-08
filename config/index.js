26974c9f110ae35dea40a39ae59697134a945b43
Cassie Durr <Cass0115@gmail.com>
2018-02-08 15:44:38 -0800

    build fix

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsPublicPath: '/',
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: '/static',
 
 
     /**
