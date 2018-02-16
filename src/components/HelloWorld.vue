068fe39bba49c22b9de2b6e97090bf69566b3bc0
Cassie Durr <Cass0115@gmail.com>
2018-02-15 19:12:05 -0800

    added API, and have it returning data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,69 +1,73 @@
 <template>
   <div class="forms">
-    <h2>{{ message }}</h2>
-    <label>Message: <input type="text" v-model="message"> </label>
-    <p>Computed reversed message: {{reversedMessage }}</p>
-    <div class="cause">
-    <h2>Selected: {{ causeSelection}} </h2>
+    <h2>Hello world</h2>
+      <!-- TODO Searchbar, location input, cause dropdown, submit button -->
+    
+      <div class="cause">
+    <h2>Selected: {{ causeSelection }} </h2>
     <label for="causeChooser">Choose a cause:</label>
     <select v-model="causeSelection">
       <option disabled value="">Please choose a Cause</option>
       <option v-for="cause in causeList" v-bind:value="cause">{{ cause }} </option>
     </select>
   </div><!-- end cause div -->
+   
+<!--     <p>Search for charities <input v-model="charity"> <button v-on:click="findCharity">Search</button></p> -->
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'FormsPractice',
-    data () {
-      return{
-       causeList: [
-       'Advocacy and Human Rights',
-       'Animals',
-       'Children and Family',
-       'Education',
-       'Health',
-       'LGBT',
-       'Religion',
-       'STEM',
-       'Women'
-       ],
-       causeSelection: 'Animals'
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      causeList: [
+        "Advocacy and Human Rights",
+        "Animals",
+        "Children and Family",
+        "Education",
+        "Health",
+        "LGBT",
+        "Religion",
+        "STEM",
+        "Women"
+      ],
+      causeSelection: "Animals",
+      errors: []
+    };
+  },
+
+  created() {
+      let charityURL ='http://crossorigin.me/http://data.orghunter.com/v1/charitysearch';
+       
+      axios.get(charityURL, {
+         params: {
+        user_key: '733478d5a8680b6d4c57b26d07d4b3fc',
+        searchTerm: 'humane',
+        city: '',
+        state: '',
+        zipCode: '',
+        eligible: 1,
+        rows: 15
+      }
+      })
+    .then(response => {
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    });
     }
   }
-}
-
-// import axios from "axios";
-
-// export default {
-//   data() {
-//     return {
-//       posts: [],
-//       errors: []
-//     }
-//   },
-
-//   created() {
-//       this.fetchData();
-//     }, 
-
-//     methods: {
-//         fetchData() {
-//             axios.get('http://data.orghunter.com/v1/charitysearch?user_key=733478d5a8680b6d4c57b26d07d4b3fc').then(response => {
-//               this.posts = response.data,
-//                 console.log('Hello');
-//             });
-//         }
-//     }
-// }
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
