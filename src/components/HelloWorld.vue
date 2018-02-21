ca4a0dbe523744408b95ef4098bd0981c1dd6405
Cassie Durr <Cass0115@gmail.com>
2018-02-20 21:28:56 -0800

    working with API

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="forms">
+  <div class="charities">
     <h2>Hello world</h2>
       <!-- TODO Searchbar, location input, cause dropdown, submit button -->
     
       <div class="cause">
     <h2>Selected: {{ causeSelection }} </h2>
-    <label for="causeChooser">Choose a cause:</label>
+    <label for="causeSelection">Choose a cause:</label>
     <select v-model="causeSelection">
       <option disabled value="">Please choose a Cause</option>
-      <option v-for="cause in causeList" v-bind:value="cause">{{ cause }} </option>
+      <option v-for="cause in causeList" v-bind:key="cause">{{ cause }} </option>
     </select>
   </div><!-- end cause div -->
    
-<!--     <p>Search for charities <input v-model="charity"> <button v-on:click="findCharity">Search</button></p> -->
+<p>Search for charities <input v-model="cause"> <button v-on:click="findCharities">Search</button></p>
+<div for="cause in causes">
+  <ul> <!-- Causes found list -->
+    <li></li>
+  </ul>
+</div>
+
   </div>
 </template>
 
@@ -35,30 +41,41 @@ export default {
         "Women"
       ],
       causeSelection: "Animals",
+      cause: [],
       errors: []
     };
   },
-
+ 
   created() {
       let charityURL ='http://crossorigin.me/http://data.orghunter.com/v1/charitysearch';
-       
-      axios.get(charityURL, {
+       axios.get(charityURL, {
          params: {
-        user_key: '733478d5a8680b6d4c57b26d07d4b3fc',
-        searchTerm: 'animal',
-        city: 'seattle',
-        state: '',
-        zipCode: '',
-        eligible: 1,
-        rows: 15
-      }
+          user_key: '733478d5a8680b6d4c57b26d07d4b3fc',
+          searchTerm: 'animal',
+          city: 'seattle',
+          state: '',
+          zipCode: '',
+          eligible: 1,
+          rows: 15
+        }
       })
     .then(response => {
-      this.posts = response.data
+      this.causes = response.data
     })
     .catch(e => {
       this.errors.push(e)
     });
+    },
+    methods: {
+      findCharities: function (){
+        axios.get(charityURL, {
+         params: {
+           user_key: '733478d5a8680b6d4c57b26d07d4b3fc',
+           searchTerm: '',
+           el: this.cause
+           }
+        })
+      }
     }
   }
 
